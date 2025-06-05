c2efad482cc17adfe7ec06e6b03ec612f7e53040
rmfnxm23 <shpark5733@gmail.com>
2025-06-05 20:32:39 +0900

    Fix: 상품 상세 페이지 옵션 선택 및 API 요청 수정 / Style: 모달 스타일 개선

--- cheesenchoco/src/features/ProductDetail/index.tsx
+++ cheesenchoco/src/features/ProductDetail/index.tsx
@@ -8,10 +8,16 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import { ProductDetailStyled } from "./styled";
+import { CustomModal, ProductDetailStyled } from "./styled";
 import Header from "@/components/Header";
 import { formatPrice } from "@/util/validation";
+import { useAuth } from "@/context/AuthContext";
+import Cookies from "js-cookie";
 
+import DOMPurify from "dompurify";
+
+import Image from "next/image";
+import remove from "@/assets/images/remove.png";
 interface productProps {
   id: number;
   name: string;
@@ -24,9 +30,25 @@ const ProductDetail = () => {
   const router = useRouter();
   const { id } = router.query;
 
+  const { user, isAuthenticated, logout } = useAuth();
+
   const [product, setProduct] = useState<productProps>();
 
   const [existingImages, setExistingImages] = useState<productProps[]>([]);
+
+  const [existingColors, setExistingColors] = useState<string[]>([]);
+
+  const [existingSize, setExistingSize] = useState<string[]>([]);
+
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const [selectedSize, setSelectedSize] = useState("");
+
+  const [selectedOptions, setSelectedOptions] = useState<
+    { color: string; size: string; price: number; quantity: number }[]
+  >([]);
+
+  const [totalPrice, setTotalPrice] = useState("");
 
   useEffect(() => {
     const getUpdateItem = async (id: any) => {
@@ -37,9 +59,10 @@ const ProductDetail = () => {
           `http://localhost:5000/admin/product/${id}`
         );
         const item = res.data.data;
-        console.log(typeof item.price, "123");
         setProduct(item);
         setExistingImages(item.imgUrls || []);
+        setExistingColors(item.colorArray || []);
+        setExistingSize(item.sizeArray || []);
       } catch (err) {
         console.error(err);
       }
@@ -47,6 +70,113 @@ const ProductDetail = () => {
     getUpdateItem(id);
   }, [id]);
   const BASE_URL = "http://localhost:5000";
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // 장바구니 추가
+  const handleCart = async (id: any) => {
+    // const data = { id, selectedColor, selectedSize };
+    // const data = {id, selectedOptions.color}
+    // console.log(selectedOptions, "data 변경");
+    // return;
+
+    if (selectedOptions.length === 0) {
+      alert("옵션을 선택해주세요.");
+      return;
+    }
+
+    const data = {
+      id: id,
+      options: selectedOptions.map((opt) => ({
+        selectColor: opt.color,
+        selectSize: opt.size,
+        quantity: opt.quantity,
+        price: opt.price, // optional
+      })),
+    };
+
+    console.log("보낼 데이터:", data);
+    // return;
+
+    try {
+      const accessToken = Cookies.get("accessToken");
+
+      const res = await axios.post(`${BASE_URL}/cart/register`, data, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      // if (res.data) alert(res.data.message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // const optionCheck = () => {
+  //   if (!selectedColor || !selectedSize) {
+  //     return alert("옵션을 선택해주세요");
+  //   }
+  // };
+
+  const addSelectedOption = () => {
+    if (!selectedColor || !selectedSize) {
+      alert("옵션을 선택해주세요");
+      return;
+    }
+
+    // 중복 방지
+    const isDuplicate = selectedOptions.some(
+      (opt) => opt.color === selectedColor && opt.size === selectedSize
+    );
+    if (isDuplicate) {
+      alert("이미 선택된 옵션입니다.");
+      return;
+    }
+    console.log(product?.price, "가격");
+    const newOption = {
+      color: selectedColor,
+      size: selectedSize,
+      price: product?.price || 0,
+      quantity: 1, // 초기 수량 1로 추가
+    };
+    setSelectedOptions((prev) => [...prev, newOption]);
+  };
+
+  // 선택된 상품을 자동 추가
+  useEffect(() => {
+    if (!selectedColor || !selectedSize || !product) return;
+    if (!selectedOptions) return;
+
+    const isDuplicate = selectedOptions.some(
+      (opt) => opt.color === selectedColor && opt.size === selectedSize
+    );
+
+    if (!isDuplicate) {
+      setSelectedOptions((prev) => [
+        ...prev,
+        {
+          color: selectedColor,
+          size: selectedSize,
+          price: product.price,
+          quantity: 1, // 초기 수량 1로 추가
+        },
+      ]);
+    }
+
+    console.log(selectedOptions, "수량 있니");
+    // ✅ 선택 후 select 초기화
+    setSelectedColor("");
+    setSelectedSize("");
+  }, [selectedColor, selectedSize]);
+
+  // 총 가격 계산
+  useEffect(() => {
+    const total = selectedOptions.reduce(
+      (sum, opt) => sum + opt.price * opt.quantity,
+      0
+    );
+    setTotalPrice(formatPrice(total));
+  }, [selectedOptions]);
+
   return (
     <>
       <Header />
@@ -71,7 +201,7 @@ const ProductDetail = () => {
                     {existingImages.map((src, index) => (
                       <SwiperSlide key={`existing-${index}`}>
                         <img
-                          src={`${BASE_URL}/uploads/${src}`}
+                          src={`${BASE_URL}/uploads/product/${src}`}
                           alt={`기존 이미지-${index}`}
                           style={{ width: "100%", height: "auto" }}
                         />
@@ -79,7 +209,14 @@ const ProductDetail = () => {
                     ))}
                   </Swiper>
                 </div>
-                <div className="product-content">{product?.content}</div>
+                <div
+                  className="product-content"
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(product?.content || ""),
+                  }}
+                >
+                  {/* {product?.content} */}
+                </div>
               </div>
 
               <div className="product-info">
@@ -88,20 +225,129 @@ const ProductDetail = () => {
                     <strong>{product?.name}</strong>
                   </div>
 
+                  {/* 상품 판매 금액 */}
                   <div className="product-price">
                     {formatPrice(product?.price)} 원
                   </div>
+
+                  {/* 색상 옵션 */}
+                  <div className="product-option">
+                    {existingColors.length >= 1 && (
+                      <select
+                        name="color"
+                        value={selectedColor}
+                        onChange={(e) => {
+                          setSelectedColor(e.target.value);
+                        }}
+                      >
+                        <option value="">color</option>
+                        {existingColors.map((x) => (
+                          <option value={x} key={x}>
+                            {x}
+                          </option>
+                        ))}
+                      </select>
+                    )}
+
+                    {/* 사이즈 옵션 */}
+                    {existingSize.length >= 1 && (
+                      <select
+                        name="size"
+                        value={selectedSize}
+                        onChange={(e) => {
+                          setSelectedSize(e.target.value);
+                        }}
+                      >
+                        <option value="">size</option>
+                        {existingSize.map((x) => (
+                          <option value={x} key={x}>
+                            {x}
+                          </option>
+                        ))}
+                      </select>
+                    )}
+                  </div>
+
+                  {/* 선택된 상품 자동 추가 */}
+                  <div
+                    className={clsx("selected-products", {
+                      active: selectedOptions.length > 0,
+                    })}
+                  >
+                    <ul>
+                      {selectedOptions.map((opt, idx) => (
+                        <li key={idx}>
+                          <p>
+                            {opt.color} / {opt.size}
+                          </p>
+                          <div className="item-calculate">
+                            <div>{opt.quantity}</div>
+                            <div>{formatPrice(opt.price)} 원</div>
+                            <div className="add-product-cancel">
+                              <Image src={remove} alt="상품 추가 삭제" />
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  {/* 선택된 상품 총 금액 */}
+                  {selectedOptions.length > 0 && (
+                    <div className="total-price">{totalPrice} 원</div>
+                  )}
+
+                  {/* 카트 or 결제 버튼 */}
                   <div className="product-button">
                     <div
                       className="action-cart"
                       onClick={() => {
-                        alert("장바구니에 담겼습니다.");
-                        router.push("/cart");
+                        if (selectedOptions) {
+                          handleCart(id);
+                          setIsModalOpen(true);
+                        } else {
+                          // optionCheck();
+                          addSelectedOption();
+                        }
                       }}
                     >
                       CART
                     </div>
-                    <div className="action-buy">BUY NOW</div>
+
+                    <CustomModal
+                      open={isModalOpen}
+                      onCancel={() => setIsModalOpen(false)}
+                      centered
+                      footer={
+                        <div className="ant-button">
+                          <button
+                            className="shop-btn"
+                            onClick={() => setIsModalOpen(false)}
+                          >
+                            쇼핑 계속하기
+                          </button>
+                          <button
+                            className="cart-btn"
+                            onClick={() => router.push("/cart")}
+                          >
+                            장바구니로 이동
+                          </button>
+                        </div>
+                      }
+                    >
+                      <p>선택하신 상품이 장바구니에 담겼습니다.</p>
+                    </CustomModal>
+
+                    <div
+                      className="action-buy"
+                      onClick={() => {
+                        isAuthenticated
+                          ? router.push("/cart")
+                          : router.push("/login");
+                      }}
+                    >
+                      BUY NOW
+                    </div>
                   </div>
                 </div>
               </div>
--- cheesenchoco/src/features/ProductDetail/styled.ts
+++ cheesenchoco/src/features/ProductDetail/styled.ts
@@ -1,3 +1,4 @@
+import { Modal } from "antd";
 import styled from "styled-components";
 
 export const ProductDetailStyled = styled.div`
@@ -23,6 +24,7 @@ export const ProductDetailStyled = styled.div`
 
       .img-wrap {
         width: 100%;
+        margin-top: 20px;
       }
 
       .img-wrap img {
@@ -74,11 +76,92 @@ export const ProductDetailStyled = styled.div`
       position: sticky;
       top: 120px; // 🧠 header 높이에 맞춰 조정
       width: 100%;
+      padding: 5px;
     }
 
     .product-name {
       border-bottom: 1px solid;
       padding: 10px 0;
+      font-size: 20px;
+    }
+
+    .product-price {
+      margin: 20px 0;
+    }
+
+    .product-option {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+
+      select {
+        width: 100%;
+        padding: 12px;
+        margin-bottom: 20px;
+        border: 1px solid #ccc;
+        outline: none;
+        box-shadow: none;
+        box-sizing: border-box;
+        max-width: 100%; // 너비 초과 방지
+
+        &:focus {
+          border: 1px solid #ccc;
+          outline: none;
+          box-shadow: none;
+        }
+
+        option {
+          width: 100%;
+          white-space: nowrap; // 텍스트 줄바꿈 방지
+          overflow: hidden; // 넘치는 텍스트 감추기
+          text-overflow: ellipsis; // 생략 부호로 처리
+        }
+      }
+    }
+
+    /* 선택된 상품 자동 추가 */
+    .selected-products {
+      width: 100%;
+      max-height: 280px;
+      font-family: "ProximaNova-Regular", "Noto Sans KR";
+      padding: 0 15px;
+      /* border-top: 1px solid #f0f0f0; */
+      /* border-bottom: 1px solid #f0f0f0; */
+      background: #fafafa;
+
+      &.active {
+        border-top: 1px solid #ccc;
+        border-bottom: 1px solid #ccc;
+      }
+
+      li {
+        position: relative;
+        padding: 20px 0;
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+      }
+
+      .item-calculate {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+
+        .add-product-cancel {
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+
+    /* 선택한 상품 총 금액 */
+    .total-price {
+      display: flex;
+      justify-content: right;
+      border-top: 2px solid black;
+      padding: 20px 0;
+      color: red;
+      font-size: 1.2rem;
     }
 
     .product-button {
@@ -92,9 +175,52 @@ export const ProductDetailStyled = styled.div`
         padding: 10px 50px;
         border: 1px solid;
         text-align: center;
+        cursor: pointer;
       }
+
       .action-cart {
         border-right: none;
+      }
+    }
+  }
+`;
+
+export const CustomModal = styled(Modal)`
+  .ant-modal-content {
+    /* border-radius: none !important; */
+    background-color: #f5f5f5;
+    padding: 40px 40px 50px;
+  }
+
+  .ant-modal-body {
+    /* background: #fff0f6; */
+    border-bottom: none;
+    padding: 30px;
+    text-align: center;
+    font-size: 20px;
+  }
+
+  .ant-modal-footer {
+    /* text-align: center; */
+    margin-top: 20px;
+
+    .ant-button {
+      display: flex;
+      justify-content: center;
+      gap: 10px;
+
+      button {
+        border-radius: none;
+        padding: 10px 20px;
+        color: #f5f5f5;
+        border: none;
+        cursor: pointer;
+      }
+      .cart-btn {
+        background-color: #7d7d7d;
+      }
+      .shop-btn {
+        background-color: #000;
       }
     }
   }
