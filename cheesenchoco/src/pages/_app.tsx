64b216f3e4f98ac5d1ff38af20de7aa932754628
rmfnxm23 <shpark5733@gmail.com>
2025-06-18 23:38:56 +0900

    Fix: Commit

--- cheesenchoco/src/pages/_app.tsx
+++ cheesenchoco/src/pages/_app.tsx
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const requiresAuth = authRequiredRoutes.some((route) =>
     router.pathname.startsWith(route)
   );
-
+  console.log(authRequiredRoutes);
   return (
     <>
       {/* <Header /> */}
